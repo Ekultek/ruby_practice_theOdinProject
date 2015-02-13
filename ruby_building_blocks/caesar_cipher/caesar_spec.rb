866e64b2f9939b2bdfc2da076e7322731d420664
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-02-13 13:44:20 -0500

    Added rspec testing

--- ruby_building_blocks/caesar_cipher/caesar_spec.rb
+++ ruby_building_blocks/caesar_cipher/caesar_spec.rb
@@ -10,4 +10,16 @@ describe "caesar_cipher" do
   it "handles shifts past z" do
     caesar_cipher("zipper", 3).should == "clsshu"
   end
+  
+  it "handles multiple words" do
+    caesar_cipher("the odin project", 4).should == "xli shmr tvsnigx"
+  end
+  
+  it "doesn't translate punctuation" do
+    caesar_cipher("Friday!&$##", 2).should == "htkfca!&$##"
+  end
+  
+  it "handles no shift" do
+    caesar_cipher("dawn", 0).should == "dawn"
+  end
 end
